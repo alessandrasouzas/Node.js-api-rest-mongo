b98bbe27c03fcfe160b540b50bd40704830c66a8
Alessandra Souza <le16bits@gmail.com>
2024-04-07 18:32:36 -0300

    cria e testa nova requisição para cadastrar livro, teste via postman

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from "express";
 
 const app = express();
+app.use(express.json()); //Função middleware do tipo json
+/*
+    Funções de Middleware tem acesso ao objeto de solicitação (req), 
+        o objeto de resposta (res), e a próxima função de middleware no ciclo solicitação-resposta do aplicativo. 
+        A próxima função middleware é comumente denotada por uma variável chamada next.
+*/
 
 const livros = [
     {
@@ -20,5 +26,11 @@ app.get("/", (req, res) => {
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("livro cadastrado com sucesso");
+});
+
 
 export default app;
