464614589499bf7f488ff4018e7e07463ff8a96f
Alessandra Souza <le16bits@gmail.com>
2024-04-07 19:02:17 -0300

    cria e testa nova requisição para remover um livro

--- src/app.js
+++ src/app.js
@@ -40,13 +40,19 @@ app.get("/livros/:id", (req, res) => {
 
 app.post("/livros", (req, res) => {
     livros.push(req.body);
-    res.status(201).send('livro cadastrado com sucesso');
+    res.status(201).send("livro cadastrado com sucesso");
 });
 
 app.put("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
     livros[index].titulo = req.body.titulo;
     res.status(200).json(livros);
+})
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("livro removido com sucesso");
 })
 
 
